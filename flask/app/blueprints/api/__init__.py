8523c481539d3b926119dbe2bd88772b83912a77
glennprays <glennsagala@gmail.com>
2023-11-15 19:11:52 +0700

    feature: file opener

--- flask/app/blueprints/api/__init__.py
+++ flask/app/blueprints/api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template_string
 
 bp = Blueprint("api", __name__, url_prefix="/api")
 
@@ -10,3 +10,16 @@ def index():
 def search_query():
     query_param = request.args.get('q', default='', type=str)
     return jsonify({"query": query_param})
+
+@bp.route("/documents/<filename>")
+def open_file(filename):
+    file_path = f"./documents/{filename}"
+
+    try:
+        with open(file_path, "r", encoding="utf-8", errors="ignore") as file:
+            text = file.read()
+
+        formatted_text = f'<pre>{text}</pre>'
+        return render_template_string(formatted_text)
+    except FileNotFoundError:
+        return "File not found", 404
